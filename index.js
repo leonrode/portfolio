33abf79d21230dc9a7eb94b98077c39a60da3247
Leon Rode <me@leonro.de>
2024-09-04 00:31:04 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const data = {
+    "projects": [
+	{
+	    "name": "Quantum Amplitude Estimation",
+	    "description": "Implemented an algorithm to estimate a basis amplitude for a quantum state",
+	    "tags": ["qiskit", "python"],
+	    "links": [{"type": "github", "link": "https://github.com/leonrode/qindia-qa101-final-project"}]
+	},
+	{
+	    "name": "Quantum Double Slit Experiment",
+	    "description": "simulated the double slit experiment on an ibm quantum computer according to historical experiment parameters. i then discussed the results in a paper",
+	    "tags": ["qiskit", "python", "latex"],
+	    "links": [{"type": "github", "link": "https://github.com/leonrode/quantum-double-slit-experiment"}]
+	},
+	{
+	    "name": "Autostereogram Research",
+	    "description": "Researched and implemented own version of computer-based autostereogram (SIRDS) generation",
+	    "tags": ["python", "p5.js"],
+	    "links": []
+	},
+	{
+	    "name": "Westchester Covid-19 Tracker",
+	    "description": "Built website as community service project to visualize COVID-19 trends in Westchester County, NY",
+	    "tags": ["python", "aws", "react.js", "mongodb", "javascript"],
+	    "links": []
+	},
+	{
+	    "name": "Disturb The Noize",
+	    "description": "Developed and managed global social platform to connect up-and-coming musicians and producers",
+	    "tags": ["next.js", "react.js", "tailwindcss", "mongodb"],
+	    "links": [{"type": "link", "link": "https://disturbthenoize.com"}]
+	}
+    ]
+};
+
+const projectsStart = document.getElementById("projects-start");
+const skills = document.getElementById("skills");
+
+for (const project of data.projects.reverse()) {
+    let linksHTML = "";
+    for (let link of project.links) {
+	linksHTML += `<div class="project-link"><a href="${link.link}">${link.type}</a></div>`;
+    }
+
+    let HTMLtoInsert = `<section><div class="project-header"><h1 class="project-title">${project.name}</h1><div>${project.tags.map(tag => "<span>" + tag + "</span>").toString().replaceAll(",", "")}</div></div><p class="project-desc">${project.description}</p>${linksHTML}</section>`;
+
+    projectsStart.insertAdjacentHTML("afterend", HTMLtoInsert);
+}
+
+let uniqueTags = new Set();
+
+let tagsHTML = "";
+
+data.projects.forEach((project) => {
+    project.tags.forEach((tag) => {
+	uniqueTags.add(tag);
+    });
+});
+
+uniqueTags.forEach((tag) => tagsHTML += "<span>" + tag + "</span>");
+
+skills.insertAdjacentHTML("afterbegin", tagsHTML);
+
