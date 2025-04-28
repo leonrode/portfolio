e9df270ac94522f7bba2e2e0fc287ed04f4f685b
Leon Rode <leon.rode13@gmail.com>
2025-04-27 22:57:16 -0400

    rebuild using nextjs

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
+        },
+      },
+    },
+  };
